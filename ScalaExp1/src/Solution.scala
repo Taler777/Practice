9556d6a775671cc7e474506220cd6b88c8f17c7f
Alexey <taler2002@gmail.com>
2018-08-22 04:03:44 +0300

    Объявление переменных изменяемых и неизменяемых Чтение строк из файла Коллекция ArrayBuffer Цикл foreach case-классы

--- ScalaExp1/src/Solution.scala
+++ ScalaExp1/src/Solution.scala
@@ -1,3 +1,25 @@
-class Solution {
+import scala.collection.mutable.ArrayBuffer
+import scala.io.Source
+
+object Solution extends App {
+
+  case class Square(Coner1: Int, Coner2: Int, Coner3: Int, Coner4: Int)
+
+  var fileName = "src\\resource.txt"
+  var figure = ArrayBuffer.empty[Square]
+  readFile()
+
+
+  def readFile() {
+    val source = Source.fromFile(fileName, "UTF-8")
+    val lines = source.getLines().toArray
+    for (line <- lines) {
+      val coners = line.split(" ").map(_.toInt)
+      val square = Square(coners(0), coners(1), coners(2), coners(3))
+      figure += square
+
+    }
+  }
+
 
 }

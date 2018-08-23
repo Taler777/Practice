39b3e5f34730eda65ee2db4dc0f006b93b3e1dba
Alexey <taler2002@gmail.com>
2018-08-23 20:04:08 +0300

    Рефакторинг кода

--- ScalaExp1/src/Solution.scala
+++ ScalaExp1/src/Solution.scala
@@ -10,7 +10,6 @@ object Solution {
   def main(args: Array[String]): Unit = {
     val date1 = new java.util.Date()
     date1.getTime
-    //var figure = ArrayBuffer.empty[Square]
     val sumLimit = 10
     val fileName = "src\\resource.txt"
     val figure = readFile(fileName)
@@ -20,7 +19,7 @@ object Solution {
     println(date2.getTime - date1.getTime)
   }
 
-  private def readFile(fileName: String) :ArrayBuffer[Square]={
+  private def readFile(fileName: String): ArrayBuffer[Square] = {
     var figure = ArrayBuffer.empty[Square]
     val source = Source.fromFile(fileName, "UTF-8")
     val lines = source.getLines().toArray
@@ -32,32 +31,33 @@ object Solution {
     figure
   }
 
-  private def isCorrectFigure(figure: ArrayBuffer[Square], sumLimit: Int): Boolean = {
-      figure(0).coner2 + figure(1).coner1 <= sumLimit && // 1-2
-        figure(0).coner3 + figure(2).coner2 + figure(3).coner1 <= sumLimit && //1-3-4
-        figure(0).coner4 + figure(1).coner3 + figure(3).coner2 + figure(4).coner1 == sumLimit && // 1-2-4-5
-        figure(1).coner4 + figure(4).coner2 + figure(5).coner1 <= sumLimit && // 2-5-6
-        figure(2).coner3 + figure(6).coner1 <= sumLimit && // 3-7
-        figure(2).coner4 + figure(3).coner3 + figure(6).coner2 + figure(7).coner1 == sumLimit && // 3-4-7-8
-        figure(3).coner4 + figure(4).coner3 + figure(7).coner2 + figure(8).coner1 == sumLimit && // 4-5-8-9
-        figure(4).coner4 + figure(5).coner3 + figure(8).coner2 + figure(9).coner1 == sumLimit && // 5-6-9-10
-        figure(5).coner4 + figure(9).coner2 <= sumLimit && // 6-10
-        figure(6).coner4 + figure(7).coner3 + figure(10).coner1 <= sumLimit && // 7-8-11
-        figure(7).coner4 + figure(8).coner3 + figure(10).coner2 + figure(11).coner1 == sumLimit && // 8-9-11-12
-        figure(8).coner4 + figure(9).coner3 + figure(11).coner2 <= sumLimit && // 9-10-12
-        figure(10).coner4 + figure(11).coner3 <= sumLimit // 11-12
+  private def isCorrectFigure(start: Int, figure: ArrayBuffer[Square], sumLimit: Int): Boolean = {
+    figure(0).coner2 + figure(1).coner1 <= sumLimit && // 1-2
+      figure(0).coner3 + figure(2).coner2 + figure(3).coner1 <= sumLimit && //1-3-4
+      figure(0).coner4 + figure(1).coner3 + figure(3).coner2 + figure(4).coner1 == sumLimit && // 1-2-4-5
+      figure(1).coner4 + figure(4).coner2 + figure(5).coner1 <= sumLimit && // 2-5-6
+      figure(2).coner3 + figure(6).coner1 <= sumLimit && // 3-7
+      figure(2).coner4 + figure(3).coner3 + figure(6).coner2 + figure(7).coner1 == sumLimit && // 3-4-7-8
+      figure(3).coner4 + figure(4).coner3 + figure(7).coner2 + figure(8).coner1 == sumLimit && // 4-5-8-9
+      figure(4).coner4 + figure(5).coner3 + figure(8).coner2 + figure(9).coner1 == sumLimit && // 5-6-9-10
+      figure(5).coner4 + figure(9).coner2 <= sumLimit && // 6-10
+      figure(6).coner4 + figure(7).coner3 + figure(10).coner1 <= sumLimit && // 7-8-11
+      figure(7).coner4 + figure(8).coner3 + figure(10).coner2 + figure(11).coner1 == sumLimit && // 8-9-11-12
+      figure(8).coner4 + figure(9).coner3 + figure(11).coner2 <= sumLimit && // 9-10-12
+      figure(10).coner4 + figure(11).coner3 <= sumLimit // 11-12
   }
 
   private def permutation(start: Int, figure: ArrayBuffer[Square], sumLimit: Int): Unit = {
+
     if (start == figure.size - 1) {
-      if (isCorrectFigure(figure, sumLimit)) {
+      if (isCorrectFigure(start, figure, sumLimit)) {
         count += 1
         figure.foreach(square => println(square.coner1 + " " + square.coner2 + " " + square.coner3 + " " + square.coner4))
         println()
       }
     }
 
-    var i = start
+    //var i = start
     for (i <- start until figure.size) {
       val c = figure(i)
       figure(i) = figure(start)
